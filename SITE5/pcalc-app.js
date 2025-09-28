f5ed595b5685487f197b0b28ef3096b584dd6d7c
pokercalculator1 <pokercalculator1@gmail.com>
2025-09-27 22:53:53 -0300

    Update pcalc-app.js

--- SITE5/pcalc-app.js
+++ SITE5/pcalc-app.js
@@ -541,9 +541,9 @@
             </span>
             <span class="lbl">Amostras:
               <select id="eqTrials" style="background:#0b1324;color:#e5e7eb;border:none;outline:0">
-                <option value="300000">30k</option>
-                <option value="500000">50k</option>
-                <option value="100000"selected>100k</option>
+                <option value="3000">3k</option>
+                <option value="5000">50</option>
+                <option value="10000"selected>10k</option>
               </select>
             </span>
             <span class="lbl">
